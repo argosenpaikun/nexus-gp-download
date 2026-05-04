a7fa6b583e5fa80ea4cac79e2bb864a061f0c60b
Mohamed Affian bin Che Mohamed Onn <affianonn@hotmail.com>
2026-05-04 08:08:12 +0800

    Update modules

--- src/main.py
+++ src/main.py
@@ -34,48 +34,26 @@ def collect_metrics():
         score = Gauge("app_score", "Average Score", registry=registry)
         review_count = Gauge("app_reviews", "Total Reviews", registry=registry)
 
-        
+        # Rating distribution metrics
+        rating_gauge = Gauge(
+             "app_rating_distribution",
+             "Distribution of Ratings",
+             ["stars"],
+             registry=registry
+        )
 
+        for star in [1, 2, 3, 4, 5]:
+            rating_gauge.labels(stars=str(star)).set(rating_count.get(star, 0))
 
+        # Push to Pushgateway
+        push_to_gateway(PUSHGATEWAY, job=JOB_NAME, registry=registry)
 
 if __name__ == "__main__":
-    app_id = "gov.mcmc.nexus"
+      while True:
+            try:
+                  collect_metrics()
+                  print("Metrics pushed successfully")
+            except Exception as e:
+                  print("Error: ", e)
 
-    result = app(app_id)
-    print("App:", result["title"])
-    print("Downloads:", result["installs"])
-    print("Rating Count:", safe(result["ratings"]))
-    print("Score:", safe(result["score"]))
-    print("Reviews:", safe(result["reviews"]))
-    print("Released:", result["released"])
-
-    print(result.get("histogram"))
-
-    review_result, _ = reviews(
-        app_id,
-        lang="en",
-        country="my",
-        sort=Sort.NEWEST,
-        count=1000
-    )
-
-    ratings = [r["score"] for r in review_result]
-
-    rating_counts = Counter(ratings)
-
-    # Ensure all ratings exist (1–5)
-    stars = [1, 2, 3, 4, 5]
-    values = [rating_counts.get(star, 0) for star in stars]
-
-    for r in review_result:
-        print("\nUser:", r["userName"])
-        print("Rating:", r["score"])
-        print("Date:", r["at"])
-        print("Comment:", r["content"])
-
-    plt.bar(stars, values)
-    plt.title("User Rating Distribution")
-    plt.xlabel("Stars")
-    plt.ylabel("Number of Reviews")
-    plt.xticks(stars)
-    plt.show()
+            time.sleep(300) # run every 5 minutes
